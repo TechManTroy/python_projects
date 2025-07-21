05bdcba7fd681cfe0a24bc2175c1936a9f28980c
TechManTroy <troydonte93@gmail.com>
2025-07-21 14:04:10 -0400

    Update caesar_cipher.py

--- fcc_projects/exercises/caesar_cipher.py
+++ fcc_projects/exercises/caesar_cipher.py
@@ -4,28 +4,34 @@ text = 'Hello Zaira'
 # Number of positions to shift each letter
 shift = 3
 
-# Alphabet used for the cipher
- alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# Define a function to encrypt the message using Caesar cipher
+def caesar():
+  # All the lowercase letters in the English alphabet
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# Variable to store the encrypted message
- encrypted_text = ''
+  # This will store the final encrypted message
+  encrypted_text = ''
 
-# Loop through each character in the input text, converted to lowercase
- for char in text.lower():
-    
-    # If the character is a space, add it to the result as-is
-     if char == ' ':
-        encrypted_text += char
-     else:
-        # Find the index of the character in the alphabet string
-        index = alphabet.find(char)
+  # Go through each character in the message (converted to lowercase)
+  for char in text.lower():
+    # If it's a space, just add it as-is
+    if char == ' ':
+      encrypted_text += char
+    else:
+      # Find the position of the character in the alphabet
+      index = alphabet.find(char)
 
-        # Calculate the new index by shifting the current index and wrapping around with modulo
-        new_index = (index + shift) % len(alphabet)
+      # Shift the index forward by the value of `shift`, wrapping around the alphabet
+      new_index = (index + shift) % len(alphabet)
 
-        # Add the shifted character to the encrypted text
-        encrypted_text += alphabet[new_index]
+      # Add the new encrypted letter to the final string
+      encrypted_text += alphabet[new_index]
 
- # Print the final encrypted result
- print('encrypted text:', encrypted_text)
+  # Print both the original and encrypted message
+  print('plain text:', text)
+  print('encrypted text:', encrypted_text)
+
+# Call the function to run the Caesar cipher
+caesar()
+
 
